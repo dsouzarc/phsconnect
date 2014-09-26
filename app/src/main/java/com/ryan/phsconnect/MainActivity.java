0fa5d175dc7ba2461e3da0a33154c076a7cce97f
Ryan D'souza <dsouzarc@gmail.com>
2014-09-26 16:35:17 -0400

    Connect to wifi

--- app/src/main/java/com/ryan/phsconnect/MainActivity.java
+++ app/src/main/java/com/ryan/phsconnect/MainActivity.java
@@ -4,14 +4,67 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.webkit.WebView;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiConfiguration;
+import java.util.List;
 
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+    private Context theC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.theC = this;
+
+        /*ConnectivityManager connManager = (ConnectivityManager) theC.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (networkInfo.isConnected()) {
+            final WifiManager wifiManager = (WifiManager) theC.getSystemService(Context.WIFI_SERVICE);
+            final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+            if (connectionInfo != null && !(connectionInfo.getSSID().equals(""))) {
+                log(connectionInfo.getSSID()); //guest
+            }
+        }*/
+
+
+        /*WifiConfiguration conf = new WifiConfiguration();
+        WifiManager wifiManager = (WifiManager)theC.getSystemService(Context.WIFI_SERVICE);
+        wifiManager.addNetwork(conf);
+        List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+        for( WifiConfiguration i : list ) {
+            if(i != null && i.SSID.equals("GUEST")) {
+                wifiManager.disconnect();
+                wifiManager.enableNetwork(i.networkId, true);
+                wifiManager.reconnect();
+            }
+        }*/
+
+        WifiManager wifiManager = (WifiManager)theC.getSystemService(Context.WIFI_SERVICE);
+        WifiConfiguration wc = new WifiConfiguration();
+        wc.SSID = "\""+"GUEST"+"\""; //IMPORTANT! This should be in Quotes!!
+        //wc.priority = 40;
+        wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        int res = wifiManager.addNetwork(wc);
+        log("add Network returned " + res );
+        boolean es = wifiManager.saveConfiguration();
+        log("saveConfiguration returned " + es );
+        boolean b = wifiManager.enableNetwork(res, true);
+        log("enableNetwork returned " + b );
+
+    }
+
+    private void log(final String message) {
+        Log.e("com.ryan.phsconnect", message);
     }
 
 
